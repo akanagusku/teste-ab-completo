d354434354041f095f597ed909fb6ba0e9339541
Andre Kanagusku <andre.kanagusku@santander.com.br>
2019-08-14 12:14:07 -0300

    fixed issue on heroku deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
  
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/index.html`));
